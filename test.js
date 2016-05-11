4e53c2bf043ac6be39219a13621dcb05ec2043e8
shintakeda <shintakeda3330@yahoo.co.jp>
2016-05-11 19:41:24 +0900

    test

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 var descriptor = {
   "blocks": [
-    [" ", "%s と言う", "say", "こんにちは!"],
-    [" ", "%s とアラートする", "tell", "こんばんは!"]
+    [" ", "%s と言う", "say", "こんにちは!"]
   ],
   "menus": {}
 }
@@ -9,10 +8,7 @@ var descriptor = {
 var ext = {
   say: function(message) {
     window.alert(message);
-  };
-  tell: function(message) {
-    window.alert(message);
-  };
+  }
 };
 
 ScratchExtensions.register("ブロック群のタイトル", descriptor, ext);
