e41426dba199127901d060355f1b4eeeaab48f53
shintakeda <shintakeda3330@yahoo.co.jp>
2016-05-13 09:08:43 +0900

    test

--- test.js
+++ test.js
@@ -1,17 +1,12 @@
 var descriptor = {
   "blocks": [
-    [" ", "%s と言う", "say", "こんにちは!"],
-    [" ", "%s と伝える", "tell", "こんばんは!"]
+    [" ", "%s と言う", "say", "こんにちは!"]
   ],
   "menus": {}
 }
 
 
 ext.say = function(message) {
-    window.alert(message);
-};
-
-ext.tell = function(message) {
     window.alert(message);
 };
 
