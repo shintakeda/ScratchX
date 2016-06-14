cf93e2994bf74ab677a323f57c34f3d40323c43f
shintakeda <shintakeda3330@yahoo.co.jp>
2016-06-14 21:16:20 +0900

    test

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 var descriptor = {
   "blocks": [
-    ["R", "%s の温度は？", "temp", "192.168.1.3"],
-    [" ", "%s と伝える", "tell", "test"],
-    ['R', '東京の気温', 'temp_of_tokyo']
+    ["R", "%s の温度は？", "temp", "192.168.1.3"]
   ],
   "menus": {}
 };
@@ -10,7 +8,7 @@ var descriptor = {
 var ext = {
   temp: function(ip, callback) {
     $.ajax({
-          url:'http://192.168.1.3/temp?callback=temp',
+          url:'http://'+ip+'/temp?callback=temp',
           type:'get',
           timeout:5000,
           cache:false,
@@ -19,20 +17,6 @@ var ext = {
           complete:function(data) {
               callback($(':hidden[name="temperature"]').val());
           }
-    });
-  },
-  tell: function(t){
-    window.alert(t);
-  },
-  temp_of_tokyo:function(callback) {
-    var param = {
-      q: "tokyo",
-      appid: "ddd98c6a715c25fd4a3b45e64c34bb10",
-      units: "metric"
-    };
-    // 気象情報 API にアクセス
-    $.get("http://api.openweathermap.org/data/2.5/weather", param, function(data) {
-      callback(data.main.temp);
     });
   }
 };
