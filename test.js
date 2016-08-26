1d00c33222b36635cc11c04652c49b31d4118389
shintakeda <shintakeda3330@yahoo.co.jp>
2016-08-26 16:40:07 +0900

    test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var descriptor = {
   "blocks": [
-    ["R", "%s の温度は？", "temp", "160.204.180.252"]
+    ["R", "%s の温度は？", "temp", "160.204.180.252"],
+    ["R", "%s のCO2濃度は？", "co2", "160.204.180.129"]
   ],
   "menus": {}
 };
@@ -16,6 +17,19 @@ var ext = {
           async:false,
           complete:function(data) {
               callback($(':hidden[name="temperature"]').val());
+          }
+    });
+  },
+  co2: function(ip, callback) {
+    $.ajax({
+          url:'http://'+ip+'/co2?callback=co2',
+          type:'get',
+          timeout:5000,
+          cache:false,
+          dataType:'jsonp',
+          async:false,
+          complete:function(data) {
+              callback($(':hidden[name="co2"]').val());
           }
     });
   }
