51c585d43de5ba74132b2465a3a09bcb4d711c0a
shintakeda <shintakeda3330@yahoo.co.jp>
2016-11-05 14:27:54 +0900

    test

--- test.js
+++ test.js
@@ -1,35 +1,21 @@
 var descriptor = {
   "blocks": [
-    ["R", "%s の温度は？", "temp", "160.204.180.252"],
-    ["R", "%s のCO2濃度は？", "co2", "160.204.180.129"]
+    ["R", "%s の明かりは？", "light", "160.204.91.132"]
   ],
   "menus": {}
 };
 
 var ext = {
-  temp: function(ip, callback) {
+  light: function(ip, callback) {
     $.ajax({
-          url:'http://'+ip+'/temp?callback=temp',
+          url:'http://'+ip+':1880/light?callback=light',
           type:'get',
           timeout:5000,
           cache:false,
           dataType:'jsonp',
           async:false,
           complete:function(data) {
-              callback($(':hidden[name="temperature"]').val());
-          }
-    });
-  },
-  co2: function(ip, callback) {
-    $.ajax({
-          url:'http://'+ip+'/co2?callback=co2',
-          type:'get',
-          timeout:5000,
-          cache:false,
-          dataType:'jsonp',
-          async:false,
-          complete:function(data) {
-              callback($(':hidden[name="co2"]').val());
+              callback($(':hidden[name="light"]').val());
           }
     });
   }
